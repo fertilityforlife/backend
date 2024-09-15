157d59c8b38ff186a0ce4845b6d2165874dd0c86
panagiotisnikolaou <pan.nik636@gmail.com>
2024-09-15 10:40:21 +0100

    added env file

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import os
 import time
 import requests
 import json
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": ["http://z2z.a41.mytemp.website", "http://127.0.0.1:5000", "http://127.0.0.1:8080"]}})  # Replace with your actual frontend URL
