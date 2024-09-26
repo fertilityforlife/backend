1843357d8148c1c2be6c45f4813471d604af2bc3
panagiotisnikolaou <pan.nik636@gmail.com>
2024-09-26 16:51:32 +0100

    changes to routes

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": ["fertilityforlife.com","http://z2z.a41.mytemp.website", "http://127.0.0.1:5000", "http://127.0.0.1:8080"]}})
+CORS(app, resources={r"/api/*": {"origins": ["fertilityforlife.com", "http://127.0.0.1:5000", "http://127.0.0.1:8080"]}})
 
 app.config['ENV'] = os.getenv('FLASK_ENV')
 
